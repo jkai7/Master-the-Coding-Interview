d84d3735ed59dc9bf561be08a8e1275a38da6a0b
Jens Kai <jkaih7@gmail.com>
2019-01-10 18:11:58 -0500

    intro to BIG O

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+//BIG O - tells us how well a problem is solved (efficiency of code)
+    //what is good code? - 1.readable, 2.scalable (big o allows us to see how       scalable code is)
+
+    const nemo = ['nemo'];
+
+    function findNemo(array) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === 'nemo') {
+                console.log('found Nemo!')
+            }
+        }
+    }
+
+    findNemo(nemo);
